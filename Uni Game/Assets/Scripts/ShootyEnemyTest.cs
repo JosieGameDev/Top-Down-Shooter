696d3cf0b6dbaec3000460685e02c9cb466668b7
JosieGameDev <43572951+JosieGameDev@users.noreply.github.com>
2018-11-13 14:45:20 +0000

    enemy movement
    changes to movemnt, movement near to player, prototype

---
+++ Uni Game/Assets/Scripts/ShootyEnemyTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootyEnemyTest : MonoBehaviour {
+
+    public Transform player;
+    public float nodeDis;
+    public float upDownDis;
+    public float speed;
+
+    public Transform target;
+    private Vector2[] nodeArray = new Vector2[3];
+	// Use this for initialization
+	void Start ()
+    {
+        CreateNodes();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = Vector2.MoveTowards(gameObject.transform.position, target.position, speed * 0.1f);
+
+        if (Vector2.Distance(player.position, gameObject.transform.position) <= 3)
+        {
+            CreateNodes();
+        }
+	}
+
+    void CreateNodes()
+    {
+        nodeArray[0] = new Vector2(player.position.x - nodeDis, player.position.y);
+        nodeArray[1] = new Vector2(player.position.x - nodeDis, player.position.y + upDownDis);
+        nodeArray[2] = new Vector2(player.position.x - nodeDis, player.position.y - upDownDis);
+
+        int randIndex = Random.Range(0, 3);
+
+        target.position = nodeArray[randIndex];
+    }
+}
