4363e5911f68cd994d220c6e43939e56c58e0303
JosieGameDev <43572951+JosieGameDev@users.noreply.github.com>
2018-11-13 08:34:49 +0000

    art and anim edits
    imported improved wall sprites, added system which flips the gun at certain rotations so it doesn't point upside down.

--- Uni Game/Assets/Scripts/MouseSmoothLook2D.cs
+++ Uni Game/Assets/Scripts/MouseSmoothLook2D.cs
@@ -7,7 +7,8 @@ public class MouseSmoothLook2D : MonoBehaviour {
     public Camera theCamera;
     public float smoothing = 5.0f;
     public float adjustmentAngle = 0.0f;
-
+    public GameObject gun;
+    SpriteRenderer spriteGun;
 
 	// Use this for initialization
 	void Start () {
@@ -25,5 +26,29 @@ public class MouseSmoothLook2D : MonoBehaviour {
         Quaternion newRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotZ + adjustmentAngle));
         transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * smoothing);
 
+        //run code to flip the gun if needed
+        FlipGunAnim();
+
 	}
+
+    void FlipGunAnim()
+
+
+    {
+
+        //get srpite of gun 
+        spriteGun = GetComponent<SpriteRenderer>();
+        
+        //check rotation 
+        if ((gun.transform.eulerAngles.z < 270 && gun.transform.eulerAngles.z > 90))
+        {
+            //flip image in these conditions
+            spriteGun.flipY = true;
+        }
+        else
+        {
+            spriteGun.flipY = false;
+        }
+                
+    }
 }
