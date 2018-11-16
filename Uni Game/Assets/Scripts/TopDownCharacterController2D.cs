6d95d90aeb5bb945bd90876ac9418c6fbc97d893
JosieGameDev <43572951+JosieGameDev@users.noreply.github.com>
2018-11-16 16:23:10 +0000

    Revert "improved shooting anim"
    This reverts commit fb5bb2647c24439305dd941913d18f0a5e7ef0d5.

--- Uni Game/Assets/Scripts/TopDownCharacterController2D.cs
+++ Uni Game/Assets/Scripts/TopDownCharacterController2D.cs
@@ -8,13 +8,11 @@ public class TopDownCharacterController2D : MonoBehaviour {
     public Rigidbody2D rigidbody2D;
     public bool isWalking;
     SpriteRenderer spriterender;
-    Animator animator;
 
 
 	// Use this for initialization
 	void Start () {
         rigidbody2D = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -25,35 +23,29 @@ public class TopDownCharacterController2D : MonoBehaviour {
         rigidbody2D.velocity = new Vector2(x, y) * speed;
         rigidbody2D.angularVelocity = 0.0f;
 
-        
         //sprite rotation 
-        //flipping based on mouse now rather than movement direction
+        spriterender = GetComponent<SpriteRenderer>();
+        if (x < 0)
+        {
+            spriterender.flipX = true;
 
-        //spriterender = GetComponent<SpriteRenderer>();
-        //if (x < 0)
-        //{
-        //    spriterender.flipX = true;
-
-        //}
-        //else if (x > 0)
-        //{
-        //    spriterender.flipX = false;
-        //}
+        }
+        else if (x > 0)
+        {
+            spriterender.flipX = false;
+        }
                 
 
 
         //setting walking state for anim purposes
 
-        if ((x != 0) || (y != 0))
+        if ((x != 0) && (y != 0))
         {
             isWalking = true;
-            
         }
         else
         {
             isWalking = false;
         }
-        Debug.Log(isWalking);
-        animator.SetBool("jackWalk", isWalking);
 	}
 }
