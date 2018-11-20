ad04bda342fbd2626aa1d832e178adbe1aaf1151
JosieGameDev <43572951+JosieGameDev@users.noreply.github.com>
2018-11-20 10:19:50 +0000

    bug fix, ranged enemy, level design
    Fixed issues with Unity and the tilepalette by recreating certain tilemaps. Created my ranged enemy which shoots at the character. Created a basic first level for the demo or die

--- Uni Game/Assets/Scripts/EnemyShooting.cs
+++ Uni Game/Assets/Scripts/EnemyShooting.cs
@@ -2,38 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyShooting : MonoBehaviour {
+public class EnemyShooting : MonoBehaviour
+{
 
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
+    public Transform playerTransform;
+    float distance;
+    public int raange = 15;
+    public float nextFire = 0;
+    public float fireRate = 0.5f;
 
     //Vector2 speed = new Vector2(10f, 10f);
 
     // Use this for initialization
-    void Start () {
-        
-    }
-	
-	// Update is called once per frame
-	void Update ()
+    void Start()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-{
-            Fire();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get distance
+        distance = Vector3.Distance(bulletSpawn.position, playerTransform.position);
+        //check if in range
+        if (distance < raange)
+        {
+
+            if (Time.time > nextFire)
+            {
+                nextFire = Time.time + fireRate;
+                Fire();
+            }
+
+            //if(Input.GetKeyDown(KeyCode.Space))
+            //{
+            //    Fire();
+            //}
+
+
+
         }
 
 
+        //void Fire()
+        //{
+        //    //Instantiate(BulletObject);
+        //    //Rigidbody2D bulletRigid = BulletObject.AddComponent<Rigidbody2D>();
+        //    //bulletRigid.AddForce(bulletSpeed);
+
+        //}
+
+
+
 
     }
-
-    //void Fire()
-    //{
-    //    //Instantiate(BulletObject);
-    //    //Rigidbody2D bulletRigid = BulletObject.AddComponent<Rigidbody2D>();
-    //    //bulletRigid.AddForce(bulletSpeed);
-        
-    //}
-
     void Fire()
     {
         Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
