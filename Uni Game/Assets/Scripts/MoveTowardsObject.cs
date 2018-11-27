527e847420ff0e1149166a3a9101b1a07994f163
JosieGameDev <43572951+JosieGameDev@users.noreply.github.com>
2018-11-27 10:31:16 +0000

    Tutorial adjustments
    added distance calculation to the melee robots so they only move to player when it's close enough to them
    Also adjusted the size of the rooms to make them a more suitable size for the tutorial

--- Uni Game/Assets/Scripts/MoveTowardsObject.cs
+++ Uni Game/Assets/Scripts/MoveTowardsObject.cs
@@ -4,27 +4,40 @@ using UnityEngine;
 
 public class MoveTowardsObject : MonoBehaviour {
 
-    public Transform target;
+    public GameObject player;
     public float speed = 5.0f;
+    Transform transform;
+    Transform currentPos;
+    float distance;
+    public float range;
 
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+		player = GameObject.FindGameObjectWithTag("Player");
+        transform = player.transform;
+        currentPos = GetComponent<Transform>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if (target != null)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * 0.1f);
 
+        distance = Vector3.Distance(currentPos.position, transform.position);
+
+        if (player != null)
+        {
+            if (distance <= range)
+            {
+                currentPos.position = Vector3.MoveTowards(currentPos.position, transform.position, speed * 0.1f);
+            }//
+            
         }
 	}
 
-    public void SetTarget(Transform newTarget)
-    {
-        target = newTarget;
-    }
+    //public void SetTarget(Transform newTarget)
+    //{
+    //    target = newTarget;
+    //}
 
 }
